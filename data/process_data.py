58f56f4f06e4d9a5f1a3ea2cee00b26054a1c906
Philipp Nockemann <philipp@nockemann.com>
2024-05-31 21:06:24 +0200

    Solved minor problem

--- data/process_data.py
+++ data/process_data.py
@@ -78,7 +78,7 @@ def save_data(df, database_filename):
     Output:
     None
     '''
-    engine = create_engine('sqlite:///', + database_filename)
+    engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('disaster_messages', engine, index=False, if_exists='replace')
     pass  
 
